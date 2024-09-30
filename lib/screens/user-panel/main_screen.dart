b063fc095e2b5a92305bb02186aff47cbf161a16
anand kumar <anandkumar7368970125@gmail.com>
2024-10-01 01:49:38 +0530

    flutter-ecomm

---
+++ lib/screens/user-panel/main_screen.dart
@@ -0,0 +1,25 @@
+import 'package:ecomm/utils/app_constant.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: AppConstant.appMainColor,
+            title: Center(
+              child: Text(AppConstant.appMainName,
+                  style: const TextStyle(
+                    color: AppConstant.appTextColor,
+                  )),
+            )));
+  }
+}
---
+++ lib/utils/app_constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static String appMainName = "Loot Bazar";
+  static String appVersion = "1.0.0";
+  static String appCopyright = "Copyright © 2021 Loot Bazar";
+  static String appDescription = "Loot Bazar is a simple game that allows users to collect and trade items";
+  static String appPoweredBy = "Ands";
+  static const appMainColor = Color.fromRGBO(255, 0, 21, 0.3);
+  static const appSecondaryColor = Color.fromRGBO(255, 0, 21, 0.6);
+  static const appTextColor = Colors.white;
+}
